d01a22b8e9918949b6eb7429080f8483f6227a61
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-01-26 17:35:47 -0500

    Use passport auth for login / logout

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const passport = require("passport");
 
 const authController = require("../controllers/auth.controller");
 
 // Public
 router.post("/register", authController.register);
-router.post("/login", authController.login);
-router.post("/logout", authController.logout);
+router.post("/login", passport.authenticate("local", { session: false }), authController.login);
+router.post("/logout", passport.authenticate("jwt", { session: false }), authController.logout);
 
 module.exports = router;
