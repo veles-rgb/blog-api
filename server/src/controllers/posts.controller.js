e834b80a4b4d59e9d3452edb35c3c02a602dfa20
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-01-27 16:20:15 -0500

    Create posts routes controller functions
    Created getAllPosts,
            getAllUserPosts,
            getPostById,
            getPostBySlug,
            createPost,
            updatePostById,
            deletePostById,
            publishPost,
            unpublishPost,

--- server/src/controllers/posts.controller.js
+++ server/src/controllers/posts.controller.js
@@ -1,52 +1,274 @@
-async function getAllPosts(req, res, next) {
+const slugify = require("slugify");
 
+async function getAllPosts(req, res, next) {
+    try {
+        const { prisma } = await import("../lib/prisma.mjs");
+
+        const posts = await prisma.posts.findMany({
+            where: {
+                isPublished: true,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            }
+        });
+
+        return res.status(200).json({ posts });
+    } catch (error) {
+        return next(error);
+    }
+}
+
+async function getAllUserPosts(req, res, next) {
+    try {
+        const { username } = req.params;
+        const { prisma } = await import("../lib/prisma.mjs");
+
+        const posts = await prisma.posts.findMany({
+            where: {
+                isPublished: true,
+                author: {
+                    username: username,
+                },
+            },
+            include: {
+                author: {
+                    select: { username: true },
+                },
+            },
+            orderBy: {
+                createdAt: 'desc',
+            }
+        });
+
+        return res.status(200).json({ posts });
+    } catch (error) {
+        return next(error);
+    }
 }
 
 async function getPostById(req, res, next) {
+    try {
+        const { postId } = req.params;
+        const { prisma } = await import("../lib/prisma.mjs");
 
+        const post = await prisma.posts.findUnique({
+            where: {
+                id: postId,
+                isPublished: true
+            }
+        });
+
+        if (!post || !post.isPublished) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        res.status(200).json({ post });
+    } catch (error) {
+        return next(error);
+    }
 }
 
 async function getPostBySlug(req, res, next) {
+    try {
+        const { username, slug } = req.params;
+        const { prisma } = await import("../lib/prisma.mjs");
 
+        const post = await prisma.posts.findFirst({
+            where: {
+                slug: slug,
+                isPublished: true,
+                author: {
+                    username: username,
+                },
+            },
+            include: {
+                author: {
+                    select: { username: true },
+                },
+            },
+        });
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        return res.status(200).json({ post });
+    } catch (error) {
+        return next(error);
+    }
 }
 
 async function createPost(req, res, next) {
-    // Must be user or admin check
-    // Get current user ID
-    // Create post in DB with user ID
+    try {
+        const user = req.user;
+
+        // Author or admin check
+        if (!user.isAuthor && !user.isAdmin) {
+            return res.status(403).json({ message: "You are not an author or admin." });
+        }
+
+        const { title, content } = req.body;
+        const { prisma } = await import("../lib/prisma.mjs");
+
+        const post = await prisma.posts.create({
+            data: {
+                authorId: user.id,
+                title,
+                content,
+                isPublished: false,
+                slug: slugify(title, { lower: true, strict: true }),
+            },
+        });
+
+        return res.status(201).json({ post });
+    } catch (error) {
+        return next(error);
+    }
 }
 
 async function updatePostById(req, res, next) {
-    // Must be user or admin check
-    // Get current user ID
-    // Update post in DB
+    const user = req.user;
+    try {
+        const { postId } = req.params;
+        let { title, content } = req.body || {};
+        const { prisma } = await import("../lib/prisma.mjs");
+
+        // Get post from ID
+        const post = await prisma.posts.findUnique({
+            where: { id: postId }
+        });
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        if (!user.isAdmin && post.authorId !== user.id) {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+
+        if (!title) title = post.title;
+        if (!content) content = post.content;
+
+        const data = { title, content };
+
+        // only update slug if title changed
+        if (title !== post.title) {
+            data.slug = slugify(title, { lower: true, strict: true });
+        }
+
+        const updatedPost = await prisma.posts.update({
+            where: { id: postId },
+            data,
+        });
+
+        return res.status(200).json({ updatedPost });
+    } catch (error) {
+        return next(error);
+    }
 }
 
 async function deletePostById(req, res, next) {
-    // Must be user or admin check
-    // Get current user ID
-    // Delete post in DB
+    const user = req.user;
+
+    try {
+        const { postId } = req.params;
+        const { prisma } = await import("../lib/prisma.mjs");
+
+        const post = await prisma.posts.findUnique({
+            where: { id: postId }
+        });
+
+        if (!post) return res.status(404).json({ message: "Post not found" });
+
+        if (!user.isAdmin && post.authorId !== user.id) {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+
+        const deletedPost = await prisma.posts.delete({
+            where: { id: postId }
+        });
+
+        return res.status(200).json({ deletedPost });
+    } catch (error) {
+        return next(error);
+    }
 }
 
-async function updatePostPublished(req, res, next) {
-    // Must be user or admin check
-    // Check if already published
-    // Set publishedAt 
+async function publishPost(req, res, next) {
+    const user = req.user;
+    try {
+        const { postId } = req.params;
+        const { prisma } = await import("../lib/prisma.mjs");
+
+        const post = await prisma.posts.findUnique({
+            where: { id: postId }
+        });
+
+        if (!post) return res.status(404).json({ message: "Post not found" });
+
+        if (!user.isAdmin && post.authorId !== user.id) {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+
+        if (post.isPublished) return res.status(200).json({ message: "Post is already published" });
+
+        const publishedPost = await prisma.posts.update({
+            where: { id: postId },
+            data: {
+                isPublished: true,
+                publishedAt: new Date()
+            }
+        });
+
+        return res.status(200).json({ publishedPost });
+    } catch (error) {
+        return next(error);
+    }
 }
 
-async function updatePostUnpublish(req, res, next) {
-    // Must be user or admin check
-    // Check if published
-    // Clear publishedAt
+async function unpublishPost(req, res, next) {
+    const user = req.user;
+
+    try {
+        const { postId } = req.params;
+        const { prisma } = await import("../lib/prisma.mjs");
+
+        const post = await prisma.posts.findUnique({
+            where: { id: postId }
+        });
+
+        if (!post) return res.status(404).json({ message: "Post not found" });
+
+        if (!user.isAdmin && post.authorId !== user.id) {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+
+        if (!post.isPublished) return res.status(200).json({ message: "Post is already unpublished" });
+
+        const unpublishedPost = await prisma.posts.update({
+            where: { id: postId },
+            data: {
+                isPublished: false,
+                publishedAt: null,
+            }
+        });
+
+        return res.status(200).json({ unpublishedPost });
+    } catch (error) {
+        return next(error);
+
+    }
 }
 
 module.exports = {
     getAllPosts,
+    getAllUserPosts,
     getPostById,
     getPostBySlug,
     createPost,
     updatePostById,
     deletePostById,
-    updatePostPublished,
-    updatePostUnpublish,
+    publishPost,
+    unpublishPost,
 };
