718a8126af946e66c2c89f027ba6c6c274a07ae1
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-01-27 16:19:01 -0500

    Create posts routes & add JWT auth for some

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,18 +1,20 @@
 const router = require("express").Router();
+const passport = require("passport");
 
 const postsController = require("../controllers/posts.controller");
 
 // Public
 router.get("/", postsController.getAllPosts);
 router.get("/:postId", postsController.getPostById);
+router.get("/user/:username", postsController.getAllUserPosts);
 router.get("/by-slug/:username/:slug", postsController.getPostBySlug);
 
 // Protected (User or Admin)
-router.post("/", postsController.createPost);
-router.patch("/:postId", postsController.updatePostById);
-router.delete("/:postId", postsController.deletePostById);
+router.post("/", passport.authenticate("jwt", { session: false }), postsController.createPost);
+router.patch("/:postId", passport.authenticate("jwt", { session: false }), postsController.updatePostById);
+router.delete("/:postId", passport.authenticate("jwt", { session: false }), postsController.deletePostById);
 
-router.patch("/:postId/publish", postsController.updatePostPublished);
-router.patch("/:postId/unpublish", postsController.updatePostUnpublish);
+router.patch("/:postId/publish", passport.authenticate("jwt", { session: false }), postsController.publishPost);
+router.patch("/:postId/unpublish", passport.authenticate("jwt", { session: false }), postsController.unpublishPost);
 
 module.exports = router;
