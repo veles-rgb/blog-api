29d34b3aef3afefa27aabfd6b9b3ebd04a6a858c
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-02 16:43:06 -0500

    Clean up routes and add JWT auth

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 
+const passport = require("passport");
+
 const usersController = require("../controllers/users.controller");
 
 // Public
 router.get("/:userId", usersController.getUserById);
+router.get("/user/:username", usersController.getUserByUsername);
 
 // Admin
-router.get("/", usersController.getAllUsers);
-router.patch("/:userId/admin", usersController.updateAdminStatus);
-router.patch("/:userId/author", usersController.updateAuthorStatus);
-router.delete("/:userId", usersController.deleteUserById);
+router.get("/", passport.authenticate("jwt", { session: false }), usersController.getAllUsers);
+router.patch("/:userId/admin", passport.authenticate("jwt", { session: false }), usersController.updateAdminStatus);
+router.patch("/:userId/author", passport.authenticate("jwt", { session: false }), usersController.updateAuthorStatus);
+router.delete("/:userId", passport.authenticate("jwt", { session: false }), usersController.deleteUserById);
 
 module.exports = router;
