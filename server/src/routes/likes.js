963b6adca063a0a7dde65c349d77c1e9e12a88e9
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-02 15:36:02 -0500

    Clean up routes and add JWT auth

--- server/src/routes/likes.js
+++ server/src/routes/likes.js
@@ -1,15 +1,17 @@
 const router = require("express").Router();
 
+const passport = require("passport");
+
 const likesController = require("../controllers/likes.controller");
 
 // Public
 router.get("/", likesController.getPostLikesById); // /api/likes?postId=...
 
 // Protected - Logged in
-router.post("/", likesController.postLike);
+router.post("/:postId", passport.authenticate("jwt", { session: false }), likesController.postLike);
+router.delete("/:postId", passport.authenticate("jwt", { session: false }), likesController.deleteLike);
 
 // Protected - Owner or Admin
-router.get("/user/:username", likesController.getAllUserLikes);
-router.delete("/:postId", likesController.deleteLike);
+router.get("/user/:username", passport.authenticate("jwt", { session: false }), likesController.getAllUserLikes);
 
 module.exports = router;
