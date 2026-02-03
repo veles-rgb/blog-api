87a4c369813554b9077ecdfd7e5fdf6ee15d3908
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-03 15:51:24 -0500

    Create basic header with react router dom links

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/login">Login</Link> |{' '}
+        <Link to="/register">Register</Link>
+      </nav>
+    </header>
+  );
+}
