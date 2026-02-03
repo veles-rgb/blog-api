1614454aafdbca838167ff0623cf6ba9f99f54fb
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-03 15:48:39 -0500

    Import react router dom, pages and setup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header.jsx';
 
-function App() {
-  return <></>;
+import Home from './pages/Home.jsx';
+import Post from './pages/Post.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import NotFound from './pages/NotFound.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts/:username/:slug" element={<Post />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
-
-export default App;
