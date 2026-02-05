957fc0160c09d5c59ce6b58f2bab1149d0683cbd
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-05 11:36:57 -0500

    Update header layout

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,30 @@
 import { Link } from 'react-router-dom';
+import { ImUser } from 'react-icons/im';
 
-export default function Header() {
+export default function Header({ user, onLogout }) {
   return (
     <header>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/login">Login</Link> |{' '}
-        <Link to="/register">Register</Link>
+        {user ? (
+          <>
+            <Link to="/">Home</Link> |{' '}
+            <a
+              onClick={onLogout}
+              style={{ cursor: 'pointer', display: 'flex' }}
+            >
+              {' '}
+              Logout
+            </a>
+            <p>
+              <ImUser /> {user.username}
+            </p>
+          </>
+        ) : (
+          <>
+            <Link to="/">Home</Link> | <Link to="/login">Login</Link> |{' '}
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
