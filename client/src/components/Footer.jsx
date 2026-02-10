27f7c7c2ea742733a761fe3f30af01488c888a5a
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-10 14:54:23 -0500

    Create footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer style={{ color: 'var(--text-faint)' }}>
+      <p>Copyright © 2026</p>
+      <a
+        style={{ paddingLeft: '0.3rem' }}
+        href="https://github.com/veles-rgb"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        veles-rgb
+      </a>
+      . All Rights Reserved.
+    </footer>
+  );
+}
