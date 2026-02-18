40718b91b8ee2b9f1e254853c8dda210213bc40a
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-18 15:34:33 -0500

    Stop production of this page.

--- editor/src/pages/AllPosts.jsx
+++ editor/src/pages/AllPosts.jsx
@@ -1,1 +1,11 @@
-export default function AllPosts() {}
+export default function AllPosts() {
+  return (
+    <main>
+      <h1>This feature's development has been paused.</h1>
+      <p>
+        The goal of this page was to display all blog posts with CRUD actions
+        for moderation.
+      </p>
+    </main>
+  );
+}
